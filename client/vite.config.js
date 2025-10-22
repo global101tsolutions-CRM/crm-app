dde12cd8bc2abf628e9074dae69045a270989a98
global101tsolutions-CRM <global101tsolutions@gmail.com>
2025-10-22 15:16:44 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,18 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:4000"
-    }
-  }
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:4000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
