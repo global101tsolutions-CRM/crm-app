c4277423ab200a0c24f6b291752c7e8a79bfa3a5
global101tsolutions-CRM <global101tsolutions@gmail.com>
2025-10-22 16:51:12 +0200

    Delete client directory

--- client/src/api.js
+++
@@ -1,9 +0,0 @@
-
-export async function api(path, opts={}) {
-  const res = await fetch(path, {
-    headers: { "Content-Type": "application/json" },
-    ...opts,
-  });
-  if (!res.ok) throw new Error((await res.json()).error?.message || res.statusText);
-  return (await res.json()).data;
-}
