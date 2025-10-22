48aea99e0c214c7c6de4d228cb3de445163046fe
global101tsolutions-CRM <global101tsolutions@gmail.com>
2025-10-22 16:51:22 +0200

    Delete server directory

--- server/src/utils/responses.js
+++
@@ -1,9 +0,0 @@
-export const ok = (res, data) => res.json({ data });
-
-export const err = (res, code, message) =>
-  res.status(code).json({ error: { message } });
-
-export const notFound = (res, message = "Not found") => err(res, 404, message);
-
-export const badRequest = (res, message = "Bad request") =>
-  err(res, 400, message);
--- server/src/utils/validation.js
+++
@@ -1,2 +0,0 @@
-export const formatZodError = (error) =>
-  error.issues.map((issue) => issue.message).join(", ");
